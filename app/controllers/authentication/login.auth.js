8621e19f079626be1e6f0d273ab8a79b90793c3b
Omar Essam <omaressam0858@gmail.com>
2024-02-01 04:42:13 +0200

    Revert "Modifying login endpoint"
    This reverts commit accbab1573e873d393a9ddcc75983b1069d12296.

--- app/controllers/authentication/login.auth.js
+++ app/controllers/authentication/login.auth.js
@@ -17,7 +17,7 @@ export default async function loginController(req, res, next) {
         if(!isMatch) return res.status(400).json({message: "Invalid credentials"});
 
         const token = jwt.sign({id: user.id,roleId:user.roleId}, JWT_SECRET, {expiresIn: '1d'});
-        res.status(200).json({token,roleId:user.roleId});
+        res.status(200).json({token});
     }catch(err){
         next(err)
     }
