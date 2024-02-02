6f40afa59a3fd8412067c7456ef4d67cd9ebc059
Omar Essam <omaressam0858@gmail.com>
2024-02-03 00:34:07 +0200

    Longer Reports

--- app/utils/validations/report.validation.js
+++ app/utils/validations/report.validation.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const reportSchema = joi.object({
     title: joi.string().required().min(3).max(64),
-    description: joi.string().required().min(3).max(512)
+    description: joi.string().required().min(3).max(1024)
 })
 
 export default reportSchema;
