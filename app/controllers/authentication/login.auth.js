7a81b66a36013da0c78816010d8cdc74834fb535
Omar Essam <omaressam0858@gmail.com>
2024-02-08 03:06:47 +0200

    First Login Feature.

--- app/controllers/authentication/login.auth.js
+++ app/controllers/authentication/login.auth.js
@@ -2,7 +2,7 @@ import { User } from "../../models/index.js";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { JWT_SECRET } from "../../config.js";
-import loginSchema from "../../utils/validations/login.validation.js";
+import {loginSchema , firstLoginSchema} from "../../utils/validations/login.validation.js";
 
 export default async function loginController(req, res, next) {
     try{
@@ -15,9 +15,27 @@ export default async function loginController(req, res, next) {
 
         const isMatch = await bcrypt.compare(password, user.password);
         if(!isMatch) return res.status(400).json({message: "Invalid credentials"});
+        
+        if (user.firstLogin == true) 
+        {
+            const newPassword = req.body.newPassword;
+            if(!newPassword)
+                return res.status(406).json({message: "New password is required"}); 
+            const {error} = firstLoginSchema.validate(req.body);
+            if(error) return res.status(400).json({message: error.message});
 
-        const token = jwt.sign({id: user.id,roleId:user.roleId}, JWT_SECRET, {expiresIn: '1d'});
-        res.status(200).json({token, roleId: user.roleId,name:user.name});
+            user.password = newPassword;
+            user.firstLogin = false;
+            await user.save();
+            const token = jwt.sign({id: user.id,roleId:user.roleId}, JWT_SECRET, {expiresIn: '1d'});
+            res.status(200).json({token, roleId: user.roleId,name:user.name});
+        }
+        else
+        {
+            const token = jwt.sign({id: user.id,roleId:user.roleId}, JWT_SECRET, {expiresIn: '1d'});
+            res.status(200).json({token, roleId: user.roleId,name:user.name});    
+        }
+
     }catch(err){
         next(err)
     }
--- app/utils/validations/login.validation.js
+++ app/utils/validations/login.validation.js
@@ -2,7 +2,13 @@ import joi from 'joi';
 
 const loginSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required().min(8).max(32)
+    password: joi.string().required().min(8).max(32),
+    newPassword: joi.string().min(8).max(32)
 })
 
-export default loginSchema;
+const firstLoginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required().min(8).max(32),
+    newPassword: joi.string().required().min(8).max(32)
+})
+export {loginSchema, firstLoginSchema};
